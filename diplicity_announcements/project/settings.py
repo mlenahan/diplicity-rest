c8a9bf1bc7d11005b4cccf3db4add8f3da7b5ca3
Michael Lenahan <michael_lenahan@hotmail.com>
2021-12-02 12:55:17 +0000

    Add env file for secret key

--- diplicity_announcements/project/settings.py
+++ diplicity_announcements/project/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import dj_database_url
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gl!d!p++3j5c2if#+qb^(q5!(sk%q28x8y#-i9oy@k#1id=r$q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
